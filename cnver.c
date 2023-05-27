126b706b35e1d12f235291689cb769c8249de15a
sara yamil <sarayamil18@gmail.com>
2023-05-27 16:39:09 -0700

    sara

---
+++ cnver.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * convert - conver function by sara and ghizlane
+ * @num: input numero
+ * @base: input basice 
+ * @lowercase: hexa flag for this function hhhhhhhhhhhh
+ * Return: maso9akch a betty
+ */
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rp;
+	static char buf[50];
+	char *p;
+
+	rp = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	p = &buf[49];
+	*p = '\0';
+	do {
+		*--p = rp[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (p);
+}
