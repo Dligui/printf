b54de5016524ac32fa7bd8b951a27b76797daa32
sara yamil <sarayamil18@gmail.com>
2023-05-27 16:52:52 -0700

    sara

--- _printf.c
+++ _printf.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * _printf - fonction katpranty
+ * @format: char constant pointer
+ * Return: maso9akch
  */
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list arg;
+	flags_t flg = {0, 0, 0};
 
-	register int count = 0;
+	register int cnt = 0;
 
-	va_start(arguments, format);
+	va_start(arg, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -25,19 +25,19 @@ int _printf(const char *format, ...)
 			p++;
 			if (*p == '%')
 			{
-				count += _putchar('%');
+				cnt += _putchar('%');
 				continue;
 			}
-			while (get_flag(*p, &flags))
+			while (get_flag(*p, &flg))
 				p++;
 			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
+			cnt += (pfunc)
+				? pfunc(arg, &flg)
 				: _printf("%%%c", *p);
 		} else
-			count += _putchar(*p);
+			cnt += _putchar(*p);
 	}
 	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	va_end(arg);
+	return (cnt);
 }
