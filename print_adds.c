55ed29a3f12b1947726104b32c0d60bfedc6d359
sara yamil <sarayamil18@gmail.com>
2023-05-27 18:00:11 -0700

    sara

---
+++ print_adds.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_address - prints address for this project printf
+ * @l: va_list arguments dyal had  _printf
+ * @f: pointer dyal struct arguments a zeen
+ * Return: moso9akch hhhhhh kimba
+ */
+int print_address(va_list l, flags_t *f)
+{
+	char *st;
+	unsigned long int j = va_arg(l, unsigned long int);
+
+	register int cnt = 0;
+
+	(void)f;
+
+	if (!j)
+		return (_puts("(nil)"));
+	st = convert(p, 16, 1);
+	cnt += _puts("0x");
+	cnt += _puts(str);
+	return (cnt);
+}
